8c69c019891c1c857e9fd2a48191b8b7e739f190
Andrew Breck <31482159+ahbreck@users.noreply.github.com>
2025-10-07 20:32:53 -0400

    Completed updates to do both integer and float versions

--- Assignment_3/fig-functional-pipeline-combination.go
+++ Assignment_3/fig-functional-pipeline-combination.go
@@ -5,30 +5,49 @@ import (
 	"math/rand"
 )
 
-func main() {
-	multiply := func(values []int, multiplier int) []int {
-		multipliedValues := make([]int, len(values))
-		for i, v := range values {
-			multipliedValues[i] = v * multiplier
-		}
-		return multipliedValues
-	}
-	add := func(values []int, additive int) []int {
-		addedValues := make([]int, len(values))
-		for i, v := range values {
-			addedValues[i] = v + additive
-		}
-		return addedValues
-	}
+// Create a logical condition to use in the functions with Go generics to allow any type with an underlying type of int or float64
+type Number interface {
+	~int | ~float64
+}
 
-	//ints := []int{1, 2, 3, 4}
+// Moved the function declarations to the package area to allow use of Go generics so the functions can be more flexible to use either int or float64 inputs
+// Using Go version 1.24.4, which allows for this
+func multiply[T Number](values []T, multiplier T) []T {
+	multipliedValues := make([]T, len(values))
+	for i, v := range values {
+		multipliedValues[i] = v * multiplier
+	}
+	return multipliedValues
+}
+
+func add[T Number](values []T, additive T) []T {
+	addedValues := make([]T, len(values))
+	for i, v := range values {
+		addedValues[i] = v + additive
+	}
+	return addedValues
+}
+
+// create functions to generate integer slices and float slices
+
+func main() {
 
 	ints := make([]int, 100)
 	for i := range ints {
 		ints[i] = rand.Intn(10)
 	}
 
+	floats := make([]float64, 100)
+	for i := range floats {
+		floats[i] = rand.Float64()
+	}
+
 	for _, v := range add(multiply(ints, 2), 1) {
 		fmt.Println(v)
 	}
+
+	for _, v := range add(multiply(floats, 2), 1) {
+		fmt.Println(v)
+	}
+
 }
--- Assignment_3/go.mod
+++ Assignment_3/go.mod
@@ -1,3 +1,3 @@
-module fig-functional-pipeline-combination.go
+module github.com/ahbreck/Data_Engineering_Pilots
 
-go 1.16
+go 1.24.4
